e7136ada082407a6378d37786a642cefb1dcb3de
Jose Hernandez <andres.hernandez@techlatam.la>
2019-03-28 09:06:44 -0400

    Pull modificado

--- pull.php
+++ pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 
-shell_exec( 'cd C:\xampp\htdocs && git pull origin master ' );
+shell_exec( 'cd /home2/interli1/cronometraje.interline.com.ve/ && git pull origin master ' );
 
 
 ?>
